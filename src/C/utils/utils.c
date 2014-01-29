2c6689e61183960645d3f1afd2282d2b9f1cf724
monSpeudo <worms.rockeur@gmail.com>
2014-01-29 21:36:29 +0100

    Relecture de utils.c
    Modification mineures

--- src/C/utils/utils.c
+++ src/C/utils/utils.c
@@ -10,45 +10,52 @@
 
 
 char* HMAC_SHA1(unsigned long count, secret key, char * buffer) {
-    if ((key != NULL) && (count != -1) && (buffer != NULL)) {
-        //Copie du buffer pour les opérations XOR
-        char * cpbuffer =(char*) malloc(sizeof(char) * (key->length));
-        char * cp2buffer =(char*) malloc(sizeof(char) * (key->length));
-        unsigned char hash[SHA_DIGEST_LENGTH];
-        int Bsize = key->length + 10;
-        //XOR BIT A BIT K et IPAD
-        for (int i = 0; i < key->length; i++) {
-            cpbuffer[i] = key->buffer[i]^IPAD;
-        }
-        //Allocation nouveau buffer pour concatenation
-        char* tmp= (char *) malloc(sizeof(char) * Bsize);
-        int ret = snprintf(tmp, Bsize, "%s%ld", cpbuffer, count);
-
-        //SHA1
-        SHA1((unsigned char*)tmp, ret, hash);
-        //Redefinition de la taille du buffer de passage
-        Bsize = key->length + 20;
-        //XOR BIT A BIT K et OPAD
-        for (int i = 0; i < key->length; i++) {
-            cp2buffer[i] = key->buffer[i]^OPAD;
-        }
-        //Allocation nouveau buffer pour concatenation
-        char* tmp2= (char *) malloc(sizeof(char) * Bsize);
-        ret = snprintf(tmp2, Bsize, "%s%ld", hash, count);
-        SHA1((unsigned char*)tmp2, ret, hash);
-
-        strncpy(buffer, (const char*)hash, 20);
-        
-        //Libération ressources mémoire.
-        free(tmp);
-        free(tmp2);
-        free(cpbuffer);
-        free(cp2buffer);
-        
-        return buffer;
-    } else {
+    // Vérification de préconditions
+    if (!((key != NULL) && (count != -1) && (buffer != NULL))) {
         return NULL;
     }
+
+    // Copie du buffer pour les opérations XOR
+    char * cpbuffer = (char*) malloc(sizeof(char) * (key->length));
+    char * cp2buffer = (char*) malloc(sizeof(char) * (key->length));
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    int bSize = key->length + 10;
+
+    // XOR BIT A BIT K et IPAD
+    for (int i = 0; i < key->length; i++) {
+        cpbuffer[i] = key->buffer[i] ^ IPAD;
+    }
+    // Allocation d'un nouveau buffer pour concatenation
+    char* tmp = (char *) malloc(sizeof(char) * bSize);
+
+    // snprintf renvoie le nombre de caractères écrits dans le buffer.
+    int ret = snprintf(tmp, bSize, "%s%ld", cpbuffer, count);
+
+    // SHA1
+    SHA1((unsigned char*)tmp, ret, hash);
+
+    //Redefinition de la taille du buffer de passage
+    bSize = key->length + SHA_DIGEST_LENGTH;
+
+    //XOR BIT A BIT K et OPAD
+    for (int i = 0; i < key->length; i++) {
+        cp2buffer[i] = key->buffer[i] ^ OPAD;
+    }
+
+    //Allocation nouveau buffer pour concatenation
+    char* tmp2= (char *) malloc(sizeof(char) * bSize);
+    ret = snprintf(tmp2, bSize, "%s%ld", hash, count);
+    SHA1((unsigned char*)tmp2, ret, hash);
+
+    strncpy(buffer, (const char*)hash, SHA_DIGEST_LENGTH);
+
+    //Libération ressources mémoire.
+    free(tmp);
+    free(tmp2);
+    free(cpbuffer);
+    free(cp2buffer);
+
+    return buffer;
 }
 
 char* truncate(char * hash, char * buffer) {
@@ -60,5 +67,5 @@ int32_t convert(char* buffer) {
     if(buffer == NULL) {
         return -1;
     }
-    return (int) (*buffer);
+    return (int32_t) (*buffer);
 }
