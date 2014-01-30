3a9d3eabe7c836addffdc725115d4ecdd9cd9f87
monSpeudo <worms.rockeur@gmail.com>
2014-01-30 20:02:55 +0100

    Correction mineur
    Nettoyage et reformatage

--- src/C/utils/hotp.c
+++ src/C/utils/hotp.c
@@ -9,17 +9,19 @@ int generateHOTP(secret key, unsigned long counter, int len) {
     if (!((key != NULL) && (counter > 0) && (len >= 6 && len <= 8))) {
         return -1;
     }
+
     //Allocation des buffers
     char * hmacResultBuffer = (char*) malloc(sizeof(char) * SHA_DIGEST_LENGTH);
+
     //Etape 1 HMAC
     HMAC_SHA1(counter, key, hmacResultBuffer);
-    for (int i = 0; i < SHA_DIGEST_LENGTH; i++)  {
-        printf("%02x", (unsigned char) (unsigned int) hmacResultBuffer[i]);
-    }
-    printf("\n");
+
     // Étape 2 extraction de l'OTP depuis le résultat de hmac.
     int32_t fullLengthOtp = extractOTP(hmacResultBuffer);
-    printf("%d", fullLengthOtp);
+
+    // Libération des ressources inutiles
+    free(hmacResultBuffer);
+
     //Etape 3 Human writable buffer modulo 10^len
     return fullLengthOtp % (int) pow(10,len - 1);
 }
