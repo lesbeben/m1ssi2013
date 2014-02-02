4cbc61f14887cc339b2f609f2fb2f90b0c335156
thecelticone <gaetan.ferry@gmail.com>
2014-02-02 15:06:28 +0100

    Ajout de classes Java pour les secret + maj gitignore
    Ajout d'une interface et d'une classe l'implémentant pour la
    gestion des secrets.
    Retrait des fichiers .metadata d'eclipse des commits.
    
    Signed-off-by: thecelticone <gaetan.ferry@gmail.com>

---
+++ src/Java/AndroidToken/src/utils/ISecret.java
@@ -0,0 +1,54 @@
+package utils;
+/**
+ * Une classe pour la gestion des clefs secrètes.
+ * 
+ * @post length(getSecret()) == this.length 
+ * @post length(getHexRepresentation) == 2 * this.length 
+ * @post getSecret() = hexToBinary(getHexRepresentation)
+ * @author celtic
+ *
+ */
+public interface ISecret {
+	
+	/**
+	 * Initialise le secret avec la valeur correspondant a secret.
+	 * 
+	 * @post getSecret = secret
+	 * @param secret La valeur en octet du secret.
+	 */
+	void setSecret(byte[] secret);
+	/**
+	 * Initialise le secret avec une valeur aléatoire de logueure length.
+	 * @pre length >= 0 
+	 * @post getLength = length
+	 * @param length la longueure du secret souhaité.
+	 */
+	void setSecret(int length);	
+	/**
+	 * Initialise le secret depuis une chaine hexadécimale.
+	 * @pre hexRepresentation != null 
+	 * @post getHexRepresentation = hexRep
+	 * @param hexRepresentation la chaine hexa représentant le secret.
+	 */
+	void setSecret(String hexRepresentation);
+	
+	/**
+	 * La longueure du secret courant en octet.
+	 * @pre getSecret != null
+	 * @return La longueure du secret.
+	 */
+	int getLength();	
+	/**
+	 * La valeure du secret courant en tableau d'octet.
+	 * @return Un tableau d'octet contenant la valeur du secret.
+	 */
+	byte[] getSecret();
+	/**
+	 * Une représentation human readable du secret.
+	 * 
+	 * @pre getSecret != null
+	 * @return Une chaine de caractères héxadecimaux représentant la valeur 
+	 * 		   du secret courant.
+	 */
+	String getHexRepresentation();
+}
---
+++ src/Java/AndroidToken/src/utils/Secret.java
@@ -0,0 +1,41 @@
+package utils;
+
+public class Secret implements ISecret {
+
+	@Override
+	public void setSecret(byte[] secret) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSecret(int length) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSecret(String hexRepresentation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte[] getSecret() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHexRepresentation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
