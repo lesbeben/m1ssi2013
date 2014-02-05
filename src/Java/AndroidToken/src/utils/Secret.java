b52b14a15ae5fa608eabcf688bc1b753471e97c2
claira <claire.hardouin@etu.univ-rouen.fr>
2014-02-05 18:36:57 +0100

    ajout aleatoire en fonction de la taille : Secret.java

--- src/Java/AndroidToken/src/utils/Secret.java
+++ src/Java/AndroidToken/src/utils/Secret.java
@@ -1,3 +1,5 @@
+import java.security.SecureRandom;
+
 package utils;
 
 public class Secret implements ISecret {
@@ -19,8 +21,12 @@ public class Secret implements ISecret {
 		if (length < 0) {
 			throw new IllegalArgumentException();
 		}
-
-
+		
+		//TODO
+		SecureRandom random = new SecureRandom();
+		byte bytes[] = new byte[length];
+		random.nextBytes(bytes);
+		this.secret = bytes;
 	}
 
 	@Override
@@ -30,15 +36,12 @@ public class Secret implements ISecret {
 			throw new IllegalArgumentException();
 		}
 		
-		//modifie la longueur en fonction du parametre
-		this.length = hexRepresentation.length() * 2;
-		
 		//modifie le secret
 		try {
 			Integer val = Integer.valueOf(hexRepresentation, 16); //met en decimal
 			String chiffreBin = Integer.toBinaryString(val.intValue());	//met en binaire
 			
-			this.sec = chiffreBin.getBytes(); //stocke dans sec (secret)
+			this.secret = chiffreBin.getBytes(); //stocke dans sec (secret)
 		} catch(NumberFormatException ex) {
 			ex.printStackTrace();
 		}
