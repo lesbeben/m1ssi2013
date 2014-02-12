5c3e36100981e9e808b95ec7ed23c33e2d10b420
monSpeudo <worms.rockeur@gmail.com>
2014-02-12 18:16:09 +0100

    Update utils
    Suppression des fonctions dépréciée
    Correction d'extractOTP.

--- src/C/utils/utils.h
+++ src/C/utils/utils.h
@@ -24,29 +24,6 @@
  */
 char* HMAC_SHA1(uint64_t count, secret key, char * buffer);
 
-/** Tronque un haché pour n'en retenir que 64 bits. DÉPRÉCIÉE
- * @param[in] hash le haché a tronquer.
- * @pre hash != NULL
- * @pre hash fait 160 bits.
- *
- * @param[out] buffer le buffer qui contiendra la troncature.
- * @pre buffer != NULL
- * @pre buffer fait 32 bits (8 * 8 * sizeof(char)).
- * @post buffer contient la troncature du hash. Voir troncature RFC 4226.
- *
- * @return l'adresse de buffer; NULL sinon
- */
-char* truncate(char * hash, char * buffer);
-
-/** Convertit un buffer en entier décimal. DÉPRÉCIÉE
- * @param[in] buffer le buffer à convertir.
- * @pre buffer != NULL
- * @pre buffer fait 32 bits.
- *
- * @return l'entier correspondant à la valeur du buffer; -1 sinon.
- */
-uint32_t convert(char* buffer);
-
 /** Extrait un entier d'un haché HMAC pour les générateur basés sur cette 
  * fonction. Cette fonction à pour but de remplacer truncate et convert. 
  * Plus KISS, pas de buffer de passage.
