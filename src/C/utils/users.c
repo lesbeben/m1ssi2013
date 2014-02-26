19ce4b5bdb43945a506839494a0633f35c4790ab
monSpeudo <worms.rockeur@gmail.com>
2014-02-26 19:22:17 +0100

    corrections mineures

--- src/C/utils/users.c
+++ src/C/utils/users.c
@@ -15,13 +15,23 @@ int destroyOTPUser(char* usrname) {
     if (usrname == NULL) {
         return -1;
     }
+
+    // Création d'un buffer pour accueillir le path vers le fichier de
+    // l'utilisateur.
     char path[BUFFER_SIZE];
     if (snprintf(path, BUFFER_SIZE, "%s%s", OTPWD_PATH, usrname) < 0) {
+        // Erreur non récupérable d'écriture dans un buffer.
         return -1;
     }
+
+    // Suppression des données relatives à l'utilisateur.
     if (unlink(path) < 0) {
+        // Erreur du système non récupérable à ce niveau.
         return -1;
     }
+
+    // On indique que tout s'est bien passé à l'utilisateur.
+    return 0;
 }
 
 int getOTPUser(char* usrname, otpuser* user) {
