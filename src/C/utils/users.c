9278a0ad53ed28226167a2677f8b26cfcae30ff2
michomax <maxime.michomax>
2014-02-26 19:21:41 +0100

    Modif users.s

--- src/C/utils/users.c
+++ src/C/utils/users.c
@@ -24,5 +24,33 @@ int getOTPUser(char* usrname, otpuser* user) {
 }
 
 int updateOTPUser(otpuser* user) {
-
+    if (user == NULL) {
+        return -1;
+    }
+    char path[BUFFER_SIZE];
+    if (snprintf(path, BUFFER_SIZE, "%s%s", OTPWD_PATH, user->username) < 0) {
+        return -1;
+    }
+    // Descripteur de fichier.
+    FILE * f = fopen(path, "r");
+    if (f == NULL) {
+        return -1;
+    }
+    
+    char line[BUFFER_SIZE];
+    char bufferSecret[2 * (user->passwd->length) + 1];
+    sprintf(line, "%s:%d:%s:%d\n",user->username, user->method, 
+            getHexRepresentation(user->passwd, bufferSecret, 
+                                 (2 * (user->passwd->length) + 1)),
+            user->params.count);
+    
+    if (fputs (line, f) == EOF) {
+        return -1;
+    }
+    
+    if (fclose (f) != 0) {
+        return -1;
+    }
+    
+    return 0;
 }
